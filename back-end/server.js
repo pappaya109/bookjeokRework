90379898c53e2f6e549c9b09c7e973a98b75abb3
sonsujin <sonsj33@gmail.com>
2024-01-04 12:53:37 +0900

    CHORE : 파일구조 세팅, 백엔드 라우팅

---
+++ back-end/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+
+app.set('port', process.env.PORT || 3001 );
+
+app.use(cors());
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+
+app.listen(app.get(port), ()=> {
+    console.log(waiting...)
+});
+
+
+
---
+++ front-end/src/pages/ReviewListPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReviewListPage = () => {
+    return (
+        <div>ReviewListPage</div>
+    )
+}
+
+export default ReviewListPage
